91f4f8285317efb6ffac507ad71c8d02dab359d8
Loïs <lois.puig@kctus.fr>
2016-10-15 19:41:33 +0200

    no message

--- manifests/amavis.pp
+++ manifests/amavis.pp
@@ -3,9 +3,126 @@ class ispconfig::amavis inherits ispconfig {
 	info('--- Amavisd-new, SpamAssassin, And Clamav ---')
 	info('---------------------------------------------')
 
-	ensure_packages(['amavisd-new','spamassassin','clamav','clamav-daemon','clamav-docs','zoo','unzip','bzip2','arj','nomarch','lzop','cabextract','apt-listchanges','libnet-ldap-perl','libauthen-sasl-perl','daemon','libio-string-perl','libio-socket-ssl-perl','libnet-ident-perl','zip','libnet-dns-perl ','postgrey'],
+	/*ensure_packages(['amavisd-new','spamassassin','clamav','clamav-daemon','clamav-docs','zoo','unzip','bzip2','arj','nomarch','lzop','cabextract','apt-listchanges','libnet-ldap-perl','libauthen-sasl-perl','daemon','libio-string-perl','libio-socket-ssl-perl','libnet-ident-perl','zip','libnet-dns-perl ','postgrey'], {
 		'ensure' => 'installed',
 		'require' => Exec['apt_upgrade'],
+	})*/
+
+	package { 'amavisd-new':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'spamassassin':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'clamav':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'clamav-daemon':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'clamav-docs':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'zoo':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'unzip':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	if ! defined(Package['bzip2']) {
+		package { 'bzip2':
+			ensure => 'installed',
+			require => Exec['apt_upgrade'],
+		}
+	}
+
+	package { 'arj':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'nomarch':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'lzop':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'cabextract':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'apt-listchanges':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'libnet-ldap-perl':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'libauthen-sasl-perl':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'daemon':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'libio-string-perl':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'libio-socket-ssl-perl':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'libnet-ident-perl':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'zip':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'libnet-dns-perl ':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	package { 'postgrey':
+		ensure => 'installed',
+		require => Exec['apt_upgrade'],
+	}
+
+	exec { 'spamassassin-disable':
+		path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin', '/usr/local/sbin' ],
+		command => "service spamassassin stop && systemctl disable spamassassin",
 	}
 
 	exec { 'spamassassin-disable':
