9927edfd0c4da188077a9ae7c9ffc0af67c0d1a9
Loïs <lois.puig@kctus.fr>
2016-10-15 19:36:40 +0200

    no message

--- manifests/amavis.pp
+++ manifests/amavis.pp
@@ -4,8 +4,8 @@ class ispconfig::amavis inherits ispconfig {
 	info('---------------------------------------------')
 
 	ensure_packages(['amavisd-new','spamassassin','clamav','clamav-daemon','clamav-docs','zoo','unzip','bzip2','arj','nomarch','lzop','cabextract','apt-listchanges','libnet-ldap-perl','libauthen-sasl-perl','daemon','libio-string-perl','libio-socket-ssl-perl','libnet-ident-perl','zip','libnet-dns-perl ','postgrey'],
-		ensure => 'installed',
-		require => Exec['apt_upgrade'],
+		'ensure' => 'installed',
+		'require' => Exec['apt_upgrade'],
 	}
 
 	exec { 'spamassassin-disable':
