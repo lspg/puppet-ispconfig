c13be9d9b8ce771ab7f6a4bed445e4316f6e5469
Loïs <lois.puig@kctus.fr>
2016-10-18 11:13:05 +0200

    no message

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,9 +5,10 @@ class ispconfig (
 	$admin_mail = $ispconfig::params::admin_mail,
 	$mysql_root_pwd = $ispconfig::params::mysql_root_pwd,
 ) inherits ispconfig::params  {
-	notice($::trusted.certname)
-	notice($::virtual})
-	notice($::osfamily})
+	notice($::trusted['certname'])
+	notice($::virtual)
+	notice($::osfamily)
+	notice($::trusted)
 	/*anchor { 'ispconfig::begin': } ->
 		class { '::ispconfig::sources': } ->
 		class { '::ispconfig::preliminary': } ->
