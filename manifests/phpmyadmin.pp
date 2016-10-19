9642fbcb9e7468a5035901aba967612b2345e743
Loïs <lois.puig@kctus.fr>
2016-10-19 12:47:04 +0200

    no message

--- manifests/phpmyadmin.pp
+++ manifests/phpmyadmin.pp
@@ -6,7 +6,7 @@ class ispconfig::phpmyadmin inherits ispconfig {
 		} ->
 
 		package { 'phpmyadmin':
-			require      => [ File['/tmp/phpmyadmin.preseed'], Package['apache2','php5','mariadb-server'] ],
+			require      => [ File['/tmp/phpmyadmin.preseed'], Class['apache'], Package['php5','mariadb-server'] ],
 			responsefile => "/tmp/phpmyadmin.preseed",
 			ensure       => installed,
 		}
