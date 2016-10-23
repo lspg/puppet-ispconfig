e48caa8f62b143eea065dbccb26c0cd86c52234d
Loïs <lois.puig@kctus.fr>
2016-10-23 22:55:50 +0200

    no message

--- manifests/letsencrypt.pp
+++ manifests/letsencrypt.pp
@@ -2,6 +2,6 @@ class ispconfig::letsencrypt inherits ispconfig {
 	package { 'python-certbot-apache':
 		ensure => installed,
 		install_options => '-t jessie-backports',
-		require => Exec['apt_update'],
+		require => [Apt::Pin['jessie-backports'], Exec['apt_update']],
 	}
 }
