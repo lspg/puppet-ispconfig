96c94445c024ead0a3e45248bdd00955a2e55d3f
Loïs <lois.puig@kctus.fr>
2016-10-25 15:27:29 +0200

    no message

--- manifests/test.pp
+++ manifests/test.pp
@@ -1,4 +1,9 @@
 class ispconfig::test inherits ispconfig {
+	package { 'gawk':
+		ensure => latest,
+		require => Exec['apt_update'],
+	} ->
+
 	file { '/tmp/pure-ftpd-mysql/pure-ftpd-1.0.36':
 		ensure => directory,
 		owner => 'root',
