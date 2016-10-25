bd97e3f4bc9ee85459ce4b6b8f20de53f6ea6d9e
Loïs <lois.puig@kctus.fr>
2016-10-25 20:04:58 +0200

    no message

--- lib/facter/pure-ftpd-version.rb
+++ lib/facter/pure-ftpd-version.rb
@@ -3,6 +3,6 @@ require 'facter'
 Facter.add(:pure_ftpd_version) do
     confine :kernel  => :linux
     setcode do
-        Facter::Util::Resolution.exec("V=$(apt-cache madison pure-ftpd-mysql | grep pure-ftpd-mysql | awk '{print $3}') && P=$(expr index \"$V\" -) && SH=${$V:0:$P-1} && echo ${SH}")
+        Facter::Util::Resolution.exec("apt-cache madison pure-ftpd-mysql | grep pure-ftpd-mysql | awk '{print $3}'")
     end
 end
